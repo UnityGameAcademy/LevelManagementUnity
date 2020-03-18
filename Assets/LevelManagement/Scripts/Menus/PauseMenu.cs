2066317134135c5a00493b66f85c45836f29ecc3
unitygameacademy <unitygameacademy@gmail.com>
2020-03-18 16:12:39 -0700

    GitHub: Review Pause Menu (lecture 314) updated to Unity 2019.3.5

---
+++ Assets/LevelManagement/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LevelManagement
+{
+    public class PauseMenu : Menu<PauseMenu>
+    {
+        [SerializeField]
+        private int mainMenuIndex = 0;
+
+        public void OnResumePressed()
+        {
+            Time.timeScale = 1;
+            base.OnBackPressed();
+        }
+
+        public void OnRestartPressed()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            base.OnBackPressed();
+        }
+
+        public void OnMainMenuPressed()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(mainMenuIndex);
+
+            if (MenuManager.Instance != null && MainMenu.Instance != null)
+            {
+                MenuManager.Instance.OpenMenu(MainMenu.Instance);
+            }
+        }
+
+        public void OnQuitPressed()
+        {
+            Application.Quit();
+        }
+    }
+}
