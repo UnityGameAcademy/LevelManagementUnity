eab94d64d1dcbe80dcb09d639d02bc3038cb6167
unitygameacademy <unitygameacademy@gmail.com>
2020-03-18 09:51:41 -0700

    Menu Management Part 1 (lecture 306) updated to Unity 2019.3.5

---
+++ Assets/LevelManagement/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement
+{
+    [RequireComponent(typeof(Canvas))]
+    public class Menu : MonoBehaviour
+    {
+        public void OnBackPressed()
+        {
+            
+        }
+    }
+}
---
+++ Assets/LevelManagement/Scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public Menu mainMenuPrefab;
+        public Menu settingsMenuPrefab;
+        public Menu creditsScreenPrefab;
+
+        [SerializeField]
+        private Transform _menuParent;
+
+        private void Awake()
+        {
+            InitializeMenus();
+        }
+
+        private void InitializeMenus()
+        {
+            if (_menuParent == null)
+            {
+                GameObject menuParentObject = new GameObject("Menus");
+                _menuParent = menuParentObject.transform;
+            }
+
+            Menu[] menuPrefabs = { mainMenuPrefab, settingsMenuPrefab, creditsScreenPrefab };
+
+            foreach (Menu prefab in menuPrefabs)
+            {
+                if (prefab != null)
+                {
+                    Menu menuInstance = Instantiate(prefab, _menuParent);
+                    if (prefab != mainMenuPrefab)
+                    {
+                        menuInstance.gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        // open main menu
+                    }
+                }
+            }
+        }
+
+    }
+}
