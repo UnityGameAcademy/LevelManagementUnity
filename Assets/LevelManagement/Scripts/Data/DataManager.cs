294e5f588b82d22358b602762b08beff9720d498
unitygameacademy <unitygameacademy@gmail.com>
2020-03-20 09:17:20 -0700

    Custom SaveData (lecture 504) updated to Unity 2019.3.5

---
+++ Assets/LevelManagement/Scripts/Data/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement.Data
+{
+    public class DataManager : MonoBehaviour
+    {
+        private SaveData _saveData;
+
+        public float MasterVolume
+        {
+            get { return _saveData.masterVolume; }
+            set { _saveData.masterVolume = value; }
+        }
+
+        public float SfxVolume
+        {
+            get { return _saveData.sfxVolume; }
+            set { _saveData.sfxVolume = value; }
+        }
+
+        public float MusicVolume
+        {
+            get { return _saveData.musicVolume; }
+            set { _saveData.musicVolume = value; }
+        }
+
+        private void Awake()
+        {
+            _saveData = new SaveData();
+        }
+
+
+    }
+}
---
+++ Assets/LevelManagement/Scripts/Data/SaveData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LevelManagement.Data
+{
+    public class SaveData
+    {
+        public string playerName;
+        private readonly string defaultPlayerName = "Player";
+
+        public float masterVolume;
+        public float sfxVolume;
+        public float musicVolume;
+
+        public SaveData()
+        {
+            playerName = defaultPlayerName;
+            masterVolume = 0f;
+            sfxVolume = 0f;
+            musicVolume = 0f;
+        }
+
+    }
+}
