8ce59607aebe11ffe9720e8fc7da774e9987b609
unitygameacademy <unitygameacademy@gmail.com>
2020-03-19 23:34:43 -0700

    PlayerPrefs(lecture 503) updated to Unity 2019.3.5

--- Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
+++ Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
@@ -1,33 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LevelManagement
-{ 
+{
     public class SettingsMenu : Menu<SettingsMenu>
     {
-        public void OnMasterVolumeChanged(float volume)
+        [SerializeField]
+        private Slider _masterVolumeSlider;
+
+        [SerializeField]
+        private Slider _sfxVolumeSlider;
+
+        [SerializeField]
+        private Slider _musicVolumeSlider;
+
+		protected override void Awake()
+		{
+            base.Awake();
+            LoadPreferences();
+		}
+
+		public void OnMasterVolumeChanged(float volume)
         {
-            Debug.Log("Master Volume = " + volume);
+            PlayerPrefs.SetFloat("MasterVolume", volume);
         }
 
         public void OnSFXVolumeChanged(float volume)
         {
-            
+            PlayerPrefs.SetFloat("SFXVolume", volume);
         }
 
         public void OnMusicVolumeChanged(float volume)
         {
-            
+            PlayerPrefs.SetFloat("MusicVolume", volume);
         }
 
         public override void OnBackPressed()
         {
-            // or add extra logic here
-
             base.OnBackPressed();
+            PlayerPrefs.Save();
+        }
 
-            // add extra logic here
+        public void LoadPreferences()
+        {
+            _masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+            _sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+            _musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
         }
     }
 }
