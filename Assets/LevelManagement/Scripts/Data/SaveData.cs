ca5f13fba3595603453d8b4ce133aa2e0902066a
unitygameacademy <unitygameacademy@gmail.com>
2020-03-20 15:08:10 -0700

    Hashing SaveData (lecture 509) updated to Unity 2019.3.5

--- Assets/LevelManagement/Scripts/Data/SaveData.cs
+++ Assets/LevelManagement/Scripts/Data/SaveData.cs
@@ -4,22 +4,30 @@ using System;
 
 namespace LevelManagement.Data
 {
+	// to convert to JSON, the class must be tagged with System.Serializable
     [Serializable]
     public class SaveData
     {
+    	// name for player, with default as "Player"
         public string playerName;
         private readonly string defaultPlayerName = "Player";
 
+        // floats to store sound volume levels
         public float masterVolume;
         public float sfxVolume;
         public float musicVolume;
 
+        // hash value calculated from the contents of SaveData
+        public string hashValue;
+
+        // constructor to initialize data
         public SaveData()
         {
             playerName = defaultPlayerName;
             masterVolume = 0f;
             sfxVolume = 0f;
             musicVolume = 0f;
+            hashValue = String.Empty;
         }
 
     }
