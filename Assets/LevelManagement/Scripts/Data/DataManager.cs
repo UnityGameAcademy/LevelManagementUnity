3d011831a2fc334e2e8fe1ac02b92dd0e38cd91e
unitygameacademy <unitygameacademy@gmail.com>
2020-03-20 14:49:05 -0700

    Challenge: Saving the PlayerName (lecture 507) updated to Unity 2019.3.5, corrected StartTransition

--- Assets/LevelManagement/Scripts/Data/DataManager.cs
+++ Assets/LevelManagement/Scripts/Data/DataManager.cs
@@ -9,7 +9,6 @@ namespace LevelManagement.Data
         private SaveData _saveData;
         private JsonSaver _jsonSaver;
 
-        // public properties to set and get values from the SaveData object
         public float MasterVolume
         {
             get { return _saveData.masterVolume; }
@@ -34,20 +33,17 @@ namespace LevelManagement.Data
             set { _saveData.playerName = value; }
         }
 
-        // initialize SaveData and JsonSaver objects
         private void Awake()
         {
             _saveData = new SaveData();
             _jsonSaver = new JsonSaver();
         }
 
-        // save the data using the JsonSaver
         public void Save()
         {
             _jsonSaver.Save(_saveData);
         }
 
-        // load the data using the JsonSaver
         public void Load()
         {
             _jsonSaver.Load(_saveData);
--- Assets/LevelManagement/Scripts/Data/SaveData.cs
+++ Assets/LevelManagement/Scripts/Data/SaveData.cs
@@ -4,30 +4,22 @@ using System;
 
 namespace LevelManagement.Data
 {
-	// to convert to JSON, the class must be tagged with System.Serializable
     [Serializable]
     public class SaveData
     {
-    	// name for player, with default as "Player"
         public string playerName;
         private readonly string defaultPlayerName = "Player";
 
-        // floats to store sound volume levels
         public float masterVolume;
         public float sfxVolume;
         public float musicVolume;
 
-        // hash value calculated from the contents of SaveData
-        public string hashValue;
-
-        // constructor to initialize data
         public SaveData()
         {
             playerName = defaultPlayerName;
             masterVolume = 0f;
             sfxVolume = 0f;
             musicVolume = 0f;
-            hashValue = String.Empty;
         }
 
     }
