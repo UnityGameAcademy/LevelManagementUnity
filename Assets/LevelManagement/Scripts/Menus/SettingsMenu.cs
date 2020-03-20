6ff250763ae20a63f15f187507ec9d26baa0a8f1
unitygameacademy <unitygameacademy@gmail.com>
2020-03-20 12:59:25 -0700

    Replacing PlayerPrefs (lecture 505) updated to Unity 2019.3.5

--- Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
+++ Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using LevelManagement.Data;
 
 namespace LevelManagement
 {
@@ -16,38 +17,67 @@ namespace LevelManagement
         [SerializeField]
         private Slider _musicVolumeSlider;
 
+        private DataManager _dataManager;
+
 		protected override void Awake()
 		{
             base.Awake();
-            LoadPreferences();
+            _dataManager = Object.FindObjectOfType<DataManager>();
+		}
+
+		private void Start()
+		{
+            LoadData();
 		}
 
 		public void OnMasterVolumeChanged(float volume)
         {
-            PlayerPrefs.SetFloat("MasterVolume", volume);
+            if (_dataManager != null)
+            {
+                _dataManager.MasterVolume = volume;
+            }
+            //PlayerPrefs.SetFloat("MasterVolume", volume);
         }
 
         public void OnSFXVolumeChanged(float volume)
         {
-            PlayerPrefs.SetFloat("SFXVolume", volume);
+            if (_dataManager != null)
+            {
+                _dataManager.SfxVolume = volume;
+            }
+            //PlayerPrefs.SetFloat("SFXVolume", volume);
         }
 
         public void OnMusicVolumeChanged(float volume)
         {
-            PlayerPrefs.SetFloat("MusicVolume", volume);
+            if (_dataManager != null)
+            {
+                _dataManager.MusicVolume = volume;
+            }
+            //PlayerPrefs.SetFloat("MusicVolume", volume);
         }
 
         public override void OnBackPressed()
         {
             base.OnBackPressed();
-            PlayerPrefs.Save();
+            //PlayerPrefs.Save();
         }
 
-        public void LoadPreferences()
+        public void LoadData()
         {
-            _masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
-            _sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
-            _musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+            if (_dataManager == null || _masterVolumeSlider == null ||
+                _sfxVolumeSlider == null || _musicVolumeSlider == null)
+            {
+                return;
+            }
+
+            _masterVolumeSlider.value = _dataManager.MasterVolume;
+            _sfxVolumeSlider.value = _dataManager.SfxVolume;
+            _musicVolumeSlider.value = _dataManager.MusicVolume;
+
+            //_masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+            //_sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+            //_musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
         }
     }
 }
