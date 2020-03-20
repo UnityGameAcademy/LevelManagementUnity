5ab012a9a9d450d37c28e2d2950a3043a291333e
unitygameacademy <unitygameacademy@gmail.com>
2020-03-19 23:20:49 -0700

    SettingsMenu (lecture 502) updated to Unity 2019.3.5

--- Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
+++ Assets/LevelManagement/Scripts/Menus/SettingsMenu.cs
@@ -6,6 +6,21 @@ namespace LevelManagement
 { 
     public class SettingsMenu : Menu<SettingsMenu>
     {
+        public void OnMasterVolumeChanged(float volume)
+        {
+            Debug.Log("Master Volume = " + volume);
+        }
+
+        public void OnSFXVolumeChanged(float volume)
+        {
+            
+        }
+
+        public void OnMusicVolumeChanged(float volume)
+        {
+            
+        }
+
         public override void OnBackPressed()
         {
             // or add extra logic here
